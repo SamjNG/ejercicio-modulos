2f52eac82a50668479f0c315214e99677c54aa42
SamjNG <100152735+SamjNG@users.noreply.github.com>
2022-09-22 09:34:59 -0500

    modulo operaciones

---
+++ main.py
@@ -0,0 +1,12 @@
+import operaciones
+
+
+def main():
+    op = operaciones
+    print(op.suma(2,2))
+    print(op.resta(2,2))
+    print(op.multiplicar(2,2))
+    print(op.dividir(2,2))
+
+if __name__ == "__main__" :
+    main()
